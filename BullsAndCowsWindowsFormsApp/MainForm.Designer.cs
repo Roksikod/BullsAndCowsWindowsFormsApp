b0b89c8af1e8be7822b594b0cad219e02bf78308
Роксана Залесская <ksroksana@bk.ru>
2021-08-18 22:50:09 +0300

    Добавьте файлы проекта.

---
+++ BullsAndCowsWindowsFormsApp/MainForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace BullsAndCowsWindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.puzzledWordLabel = new System.Windows.Forms.Label();
+            this.cowsCountLabel = new System.Windows.Forms.Label();
+            this.bullsCountLabel = new System.Windows.Forms.Label();
+            this.userWordTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.historyDataGridView = new System.Windows.Forms.DataGridView();
+            this.StepColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumberColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bullsCountColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cowsCountColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.historyDataGridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // puzzledWordLabel
+            // 
+            this.puzzledWordLabel.AutoSize = true;
+            this.puzzledWordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.puzzledWordLabel.Location = new System.Drawing.Point(45, 69);
+            this.puzzledWordLabel.Name = "puzzledWordLabel";
+            this.puzzledWordLabel.Size = new System.Drawing.Size(71, 20);
+            this.puzzledWordLabel.TabIndex = 0;
+            this.puzzledWordLabel.Text = "Number";
+            this.puzzledWordLabel.VisibleChanged += new System.EventHandler(this.MainForm_Load);
+            this.puzzledWordLabel.Click += new System.EventHandler(this.puzzledWordLabel_Click);
+            // 
+            // cowsCountLabel
+            // 
+            this.cowsCountLabel.AutoSize = true;
+            this.cowsCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cowsCountLabel.Location = new System.Drawing.Point(51, 249);
+            this.cowsCountLabel.Name = "cowsCountLabel";
+            this.cowsCountLabel.Size = new System.Drawing.Size(96, 24);
+            this.cowsCountLabel.TabIndex = 1;
+            this.cowsCountLabel.Text = "Cows = 0";
+            // 
+            // bullsCountLabel
+            // 
+            this.bullsCountLabel.AutoSize = true;
+            this.bullsCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bullsCountLabel.Location = new System.Drawing.Point(51, 200);
+            this.bullsCountLabel.Name = "bullsCountLabel";
+            this.bullsCountLabel.Size = new System.Drawing.Size(90, 24);
+            this.bullsCountLabel.TabIndex = 2;
+            this.bullsCountLabel.Text = "Bulls = 0";
+            // 
+            // userWordTextBox
+            // 
+            this.userWordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userWordTextBox.Location = new System.Drawing.Point(49, 135);
+            this.userWordTextBox.Name = "userWordTextBox";
+            this.userWordTextBox.Size = new System.Drawing.Size(146, 31);
+            this.userWordTextBox.TabIndex = 3;
+            this.userWordTextBox.Text = "Your number?";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(201, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // historyDataGridView
+            // 
+            this.historyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.historyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StepColumn,
+            this.NumberColumn,
+            this.bullsCountColumn,
+            this.cowsCountColumn});
+            this.historyDataGridView.Location = new System.Drawing.Point(355, 34);
+            this.historyDataGridView.Name = "historyDataGridView";
+            this.historyDataGridView.Size = new System.Drawing.Size(433, 327);
+            this.historyDataGridView.TabIndex = 5;
+            // 
+            // StepColumn
+            // 
+            this.StepColumn.HeaderText = "Step";
+            this.StepColumn.Name = "StepColumn";
+            // 
+            // NumberColumn
+            // 
+            this.NumberColumn.HeaderText = "Number";
+            this.NumberColumn.Name = "NumberColumn";
+            // 
+            // bullsCountColumn
+            // 
+            this.bullsCountColumn.HeaderText = "Bulls";
+            this.bullsCountColumn.Name = "bullsCountColumn";
+            // 
+            // cowsCountColumn
+            // 
+            this.cowsCountColumn.HeaderText = "Cows";
+            this.cowsCountColumn.Name = "cowsCountColumn";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.fileToolStripMenuItem,
+            this.rulesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(12, 20);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // rulesToolStripMenuItem
+            // 
+            this.rulesToolStripMenuItem.Name = "rulesToolStripMenuItem";
+            this.rulesToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.rulesToolStripMenuItem.Text = "Rules";
+            this.rulesToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(209, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 373);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.historyDataGridView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userWordTextBox);
+            this.Controls.Add(this.bullsCountLabel);
+            this.Controls.Add(this.cowsCountLabel);
+            this.Controls.Add(this.puzzledWordLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Bulls and Cows";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.historyDataGridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label puzzledWordLabel;
+        private System.Windows.Forms.Label cowsCountLabel;
+        private System.Windows.Forms.Label bullsCountLabel;
+        private System.Windows.Forms.TextBox userWordTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView historyDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StepColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumberColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bullsCountColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cowsCountColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rulesToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BullsAndCowsWindowsFormsApp/MainForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BullsAndCowsWindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        string puzzledWord = "";
+        const int wordLength = 4;
+        int stepCount = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<int> digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Random random = new Random();
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                int digitIndex = random.Next(0, digits.Count);
+                puzzledWord += digits[digitIndex].ToString();
+                digits.RemoveAt(digitIndex);
+            }
+            //puzzledWordLabel.Text = puzzledWord;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userWord = userWordTextBox.Text;
+
+            if (!IsValid(userWord))
+            {
+                return;
+            }
+
+            stepCount++;
+
+            int bullsCount = CalculateBullsCount(userWord);
+            int cowsCount = CalculateCowsCount(userWord);
+
+            bullsCountLabel.Text = "Bulls = " + bullsCount;
+            cowsCountLabel.Text = "Cows = " + cowsCount;
+
+            historyDataGridView.Rows.Add(stepCount, userWord, bullsCount, cowsCount);
+
+            if (bullsCount == wordLength)
+            {
+                if (stepCount <= 20)
+                {
+                    MessageBox.Show("Good job! You won in " + stepCount + " steps.");                    
+                    button1.Enabled = false;
+                    userWordTextBox.Enabled = false;
+                    puzzledWordLabel.Text = puzzledWord;
+                }
+                else
+                {
+                    MessageBox.Show("Thank you for my win! You guessed my number in " + stepCount + " steps.");                    
+                    button1.Enabled = false;
+                    userWordTextBox.Enabled = false;
+                    puzzledWordLabel.Text = puzzledWord;
+                }
+            }
+        }
+
+        private bool IsValid(string userWord)
+        {
+            if (userWord.Length != wordLength)
+            {
+                MessageBox.Show("Number contains 4 digits!");
+                return false;
+            }
+
+            for (int i = 0; i < userWord.Length; i++)
+            {
+                if (!char.IsDigit(userWord[i]))
+                {
+                    MessageBox.Show("Please use digits only!");
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < userWord.Length; i++)
+            {
+                for (int j = i + 1; j < userWord.Length; j++)
+                {
+                    if (userWord[i] == userWord[j])
+                    {
+                        MessageBox.Show("Digits are not repeated!");
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private int CalculateBullsCount(string userWord)
+        {
+            int bullsCount = 0;
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                if (puzzledWord[i] == userWord[i])
+                {
+                    bullsCount++;
+                }
+            }
+            return bullsCount;
+        }
+        private int CalculateCowsCount(string userWord)
+        {
+            int cowsCount = 0;
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                for (int j = 0; j < wordLength; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (puzzledWord[i] == userWord[j])
+                    {
+                        cowsCount++;
+                    }
+                }
+            }
+            return cowsCount;
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your aim is to guess the number (4 digits) as quickly as possible. \n " +
+                "Your hints: \n" +
+                "Bull - right digit and it is in its place. \n" +
+                "Cow - right digit, but not in this place. \n" +
+                "Zero could be the first.\n" +
+                "Have a fun!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void puzzledWordLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
